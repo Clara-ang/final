f064cf82fc74ca4471021523d8e2cbe98a2091de
Maria Clara <claramello08@gmail.com>
2023-04-12 16:34:04 -0300

    Alterações

--- progressao/settings.py
+++ progressao/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'paginas.apps.PaginasConfig',
-    'bootstrap5'
+    'bootstrap5',
    #'nomedoapp.apps.NomedoappConfig',
 ]
 
